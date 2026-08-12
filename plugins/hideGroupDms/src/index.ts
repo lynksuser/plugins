a704c7ad3ba1d940bdad142e24f6224f872246e3
lynksuser <floweyisagoodboi@gmail.com>
2026-08-11 21:59:44 -0700

    Update index.ts

--- plugins/hideGroupDms/src/index.ts
+++ plugins/hideGroupDms/src/index.ts
@@ -1,25 +1,16 @@
 import { logger } from "@vendetta";
-
 import Settings from "./Settings";
-import { patchGateway } from "./gateway";
 import { hideNow, startWatching, stopWatching } from "./localdelete";
-
-let patches: Array<() => void> = [];
 
 export default {
     onLoad() {
-        patches = patchGateway();
-
         hideNow();
-        startWatching(); // watch for server revivals
+        startWatching();
 
         logger.log("[HideGroupDMs] loaded");
     },
 
     onUnload() {
-        patches.forEach((unpatch) => unpatch?.());
-        patches = [];
-
         stopWatching();
     },
 
